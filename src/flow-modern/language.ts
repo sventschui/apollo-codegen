a3c215d7c0e7770941288e17f20679c83c8ee4ed
“Lewis <lewis@coursera.org>
2017-09-11 02:53:46 +0000

    more work

--- src/flow-modern/language.ts
+++ src/flow-modern/language.ts
@@ -3,6 +3,7 @@ import {
   GraphQLField,
   GraphQLNonNull,
   GraphQLList,
+  isAbstractType,
   isCompositeType,
   getNamedType
 } from 'graphql';
@@ -47,4 +48,72 @@ export class FlowGenerator<Context> extends CodeGenerator<Context, { typeName: s
     this.popScope();
     this.print(';');
   }
+
+  public propertyDeclarations(properties: Property[], isInput: boolean) {
+    if (!properties) return;
+    properties.forEach(property => {
+      if (isAbstractType(getNamedType(property.typeName))) {
+        console.log(property)
+      }
+    });
+  }
+  /*
+export function propertyDeclarations(generator, properties, isInput) {
+  if (!properties) return;
+  properties.forEach(property => {
+    if (isAbstractType(getNamedType(property.type || property.fieldType))) {
+      const propertySets = getPossibleTypeNames(generator, property)
+        .map(type => {
+          const inlineFragment = property.inlineFragments.find(inlineFragment => {
+            return inlineFragment.typeCondition.toString() == type
+          });
+
+          if (inlineFragment) {
+            const fields = inlineFragment.fields.map(field => {
+              if (field.fieldName === '__typename') {
+                return {
+                  ...field,
+                  typeName: `"${inlineFragment.typeCondition}"`,
+                  type: { name: `"${inlineFragment.typeCondition}"` }
+                }
+              } else {
+                return field;
+              }
+            });
+
+            return propertiesFromFields(generator, fields);
+          } else {
+            const fields = property.fields.map(field => {
+              if (field.fieldName === '__typename') {
+                return {
+                  ...field,
+                  typeName: `"${type}"`,
+                  type: { name: `"${type}"` }
+                }
+              } else {
+                return field;
+              }
+            });
+
+            return propertiesFromFields(generator, fields);
+          }
+        });
+
+      propertySetsDeclaration(generator, property, propertySets);
+    } else {
+      if (property.fields && property.fields.length > 0
+        || property.inlineFragments && property.inlineFragments.length > 0
+        || property.fragmentSpreads && property.fragmentSpreads.length > 0
+      ) {
+        propertyDeclaration(generator, property, () => {
+          const properties = propertiesFromFields(generator.context, property.fields);
+          propertyDeclarations(generator, properties, isInput);
+        });
+      } else {
+        propertyDeclaration(generator, {...property, isInput});
+      }
+    }
+  });
+}
+*/
 }
